1628293d0daf6fd0c1a2a25d3e1bd028a4684f5c
Gu-Sena <senagustavo4@gmail.com>
2023-11-07 21:42:23 -0300

    Meu Primeiro Projeto

---
+++ src/index.js
@@ -0,0 +1,28 @@
+let nomeHeroi = "Homem Aranha"
+let xpHeroi = ""
+
+
+if (xpHeroi <= 1000) {
+    console.log("O heroi " + nomeHeroi + " esta no nivel de Ferro")
+} 
+else if (xpHeroi <= 2000) {
+    console.log("O heroi " + nomeHeroi + " esta no nivel de Bronze")
+}
+else if (xpHeroi <= 5000) {
+    console.log("O heroi " + nomeHeroi + " esta no nivel de Prata")
+}
+else if (xpHeroi <= 7000) {
+    console.log("O heroi " + nomeHeroi + " esta no nivel de Ouro")
+}
+else if (xpHeroi <= 8000) {
+    console.log("O heroi " + nomeHeroi + " esta no nivel de Platina")
+}
+else if (xpHeroi <= 9000) {
+    console.log("O heroi " + nomeHeroi + " esta no nivel de Ascendente")
+}
+else if (xpHeroi <= 10000) {
+    console.log("O heroi " + nomeHeroi + " esta no nivel de Imortal")
+}
+else {
+    console.log("O heroi " + nomeHeroi + " esta no nivel de Radiante")
+}
